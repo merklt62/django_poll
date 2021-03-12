36d7a04883adb7ab7c6c80bb104ccd6f0cd4003f
Artem Lantratov <merklt62@gmail.com>
2021-03-12 20:00:39 +0300

    Update settings.py

--- django_poll/settings.py
+++ django_poll/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p2l7f7ew6p)#@ws2k1zhf+90$-6ki#jprzg$^20cy7(uq3fgiv'
+SECRET_KEY = 'add_secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
